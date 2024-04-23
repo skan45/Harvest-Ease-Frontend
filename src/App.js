9510e9112e8942cc0dd8807a0491bdd6dd463332
nour427 <nour.chaaben@insat.ucar.tn>
2024-04-23 20:31:13 +0100

    creating skelton

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Sidebar from "./ui/components/Sidebar/Sidebar";
 import CommunnityPage from "./ui/pages/CommunnityPage";
 import ChatbotPage from "./ui/pages/ChatbotPage";
 import Home from "./ui/pages/Home";
+import FarmSchedulerPage from "./ui/pages/FarmSchedulerPage";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Link to="/"></Link>
+    <div
+    style={{display:'flex'}}
+    >
       <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
-
         <Route path="/Forum" element={<CommunnityPage />} />
         <Route path="/Chatbot" element={<ChatbotPage />} />
+        <Route path="/Farm-scheduler" element={<FarmSchedulerPage/>}/>
+        
       </Routes>
     </div>
   );
---
+++ src/ui/pages/FarmSchedulerPage.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import Stack from "@mui/material/Stack";
+
+function FarmSchedulerPage() {
+  function getDate() {
+    const today = new Date();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+    const date = today.getDate();
+    return `${date}/${month}/${year}`;
+  }
+  const [currentDate, setCurrentDate] = useState(getDate());
+  return (
+    <Stack
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <Stack
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection:"column"
+        }}
+      >
+        <Stack 
+        sx={{display:"flex",flexDirection:"column" , width:"55%" }}
+        className="py-2 px-10  gap-4 absolute  top-5"
+        >
+        <p className="font-bold px-10 " >Welcome back user </p>
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              width:"100%",
+              alignItems:"center",
+              justifyContent: "space-between",
+            }}
+            className="flex justify-between"
+          >
+            <a
+              href="#"
+              className="block max-w-sm p-6 bg-slate-300 border border-gray-200 rounded-2xl shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 "
+            >
+              <p className="font-normal text-blackdark:text-gray-400">
+                "The greatest glory in living lies not in never falling, but in
+                rising every time we fall" - Nelson Mandela
+              </p>
+            </a>
+            <button style={{height:"50%"}} className="bg-green-200 text-black hover:bg-green-200 font-bold py-4 px-6 rounded-full  ">
+              Add New Task
+            </button>
+          </Stack>
+        </Stack>
+      </Stack>
+      <nav
+        className={`flex-col h-screen bg-neutral-200 w-1/4 px-7 ${
+          window.innerWidth >= 1024 ? "fixed top-0 right-0 z-10" : "hidden"
+        }`}
+      >
+        <div className="date">
+          <p className="font-normal text-center">{currentDate}</p>
+        </div>
+
+        <p className="font-bold text-center">today's schedule</p>
+      </nav>
+    </Stack>
+  );
+}
+
+export default FarmSchedulerPage;
