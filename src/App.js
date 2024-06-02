af9856f0f3fdb0bcfba718af95c01a60d8088420
nour427 <nour.chaaben@insat.ucar.tn>
2024-06-02 19:00:55 +0100

    msg

--- src/App.js
+++ src/App.js
@@ -21,11 +21,11 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/Settings" element={<SettingsPage />} />
           <Route path="/login" element={<Login/>} />
-          {/* <Route path="/Farm-scheduler" element={<FarmSchedulerPage />} /> */}
+          <Route path="/Farm-scheduler" element={<FarmSchedulerPage />} />
           <Route path="/Resources-tracker" element={<ResourcesTrackerPage />} />
           <Route path="/Forum" element={<CommunnityPage />} />
           <Route path="Chatbot" element={<ChatbotPageLayout />}>
-            {/* <Route index element={<ChatPage />} /> */}
+             <Route index element={<ChatPage />} /> 
             <Route path="Chat" element={<ChatPage />}/>
             <Route path="Saved-messages" element={<SavedMessagesPage />} />
           </Route>
