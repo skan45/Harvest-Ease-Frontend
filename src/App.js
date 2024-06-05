44415f0671feee0cd9c6b19293dba7d294734460
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-06-05 13:40:39 +0200

    Add state management with redux and configure protected routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
-
-
-
-import ChatbotPage from "./ui/pages/ChatbotPage";
 import Home from "./ui/pages/Home";
-import  ResourcesTrackerPage from "./ui/pages/ResourcesTrackerPage";
+import ResourcesTrackerPage from "./ui/pages/ResourcesTrackerPage";
 import PlantHealth from "./ui/pages/PlantHealth";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import FarmSchedulerPage from "./ui/pages/FarmSchedulerPage";
 import "./App.css";
 import Layout from "../src/ui/components/Layout";
 import SettingsPage from "../src/ui/pages/SettingsPage";
-import CommunityPage from "../src/ui/pages/CommunityPage"
+import CommunityPage from "../src/ui/pages/CommunityPage";
 import ChatPage from "../src/ui/pages/ChatbotPage";
 import SavedMessagesPage from "../src/ui/pages/SavedMessagePage";
 import ChatbotPageLayout from "../src/ui/components/chatbot/ChatbotPageLayout";
-
+import Login from "./ui/pages/login";
+import { useSelector } from "react-redux";
 function App() {
+  const isAuth = Boolean(useSelector((state) => state.token));
   return (
     <div>
       <Layout>
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/Settings" element={<SettingsPage />} />
-          <Route path="/Farm-scheduler" element={<FarmSchedulerPage />} /> 
-          <Route path="/Resources-tracker" element={<ResourcesTrackerPage />} />
-          <Route path="/Plant-Health-Scanner" element={<PlantHealth />} />
-          <Route path="/Forum" element={<CommunityPage/>} />
-          <Route path="Chatbot" element={<ChatbotPageLayout />}>
-            <Route index element={<ChatPage />} /> 
-            <Route path="Chat" element={<ChatPage />}/>
-            <Route path="Saved-messages" element={<SavedMessagesPage />} />
+          <Route
+            path="/Settings"
+            element={isAuth ? <SettingsPage /> : <Navigate to="/login" />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/Farm-scheduler"
+            element={isAuth ? <FarmSchedulerPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/Resources-tracker"
+            element={
+              isAuth ? <ResourcesTrackerPage /> : <Navigate to="/login" />
+            }
+          />
+          <Route
+            path="/Plant-Health-Scanner"
+            element={isAuth ? <PlantHealth /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/Forum"
+            element={isAuth ? <CommunityPage /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="Chatbot"
+            element={isAuth ? <ChatbotPageLayout /> : <Navigate to="/login" />}
+          >
+            <Route index element={<ChatPage />} />
+            <Route path="Chat" element={<ChatPage />} />
+            <Route
+              path="Saved-messages"
+              element={
+                isAuth ? <SavedMessagesPage /> : <Navigate to="/login" />
+              }
+            />
           </Route>
         </Routes>
       </Layout>
-
     </div>
   );
 }
-
 
 export default App;
