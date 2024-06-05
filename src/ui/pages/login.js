e235862aa0f3ee256b2689f6f91d0396c9d51646
thedevil002 <120403858+thedevil002@users.noreply.github.com>
2024-06-05 11:47:05 +0100

    dd

--- src/ui/pages/login.js
+++ src/ui/pages/login.js
@@ -1,62 +1,91 @@
 import React from "react";
 import * as Components from './Components';
-
+import axios from 'axios';
 function login() {
-    const [signIn, toggle] = React.useState(true);
-    const [isVisible, setVisible] = React.useState(false); // New state to control visibility
-    const handleVisibility = () => {
-        setVisible(!isVisible);
-    };
-     return(
-         <Components.Container>
-             <Components.SignUpContainer signinIn={signIn}>
-                 <Components.Form>
-                     <Components.Title>Create Account</Components.Title>
-                     <Components.Input type='text' placeholder='Name' />
-                     <Components.Input type='email' placeholder='Email' />
-                     <Components.Input type='password' placeholder='Password' />
-                     <Components.Button>Sign Up</Components.Button>
-                 </Components.Form>
-             </Components.SignUpContainer>
+    const [signIn, toggle] = React.useState(true); // true for sign-in, false for sign-up
 
-             <Components.SignInContainer signinIn={signIn}>
-                  <Components.Form>
-                      <Components.Title>Sign in</Components.Title>
-                      <Components.Input type='email' placeholder='Email' />
-                      <Components.Input type='password' placeholder='Password' />
-                      <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
-                      <Components.Button>Sigin In</Components.Button>
-                  </Components.Form>
-             </Components.SignInContainer>
+    function handleLogin(event) {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
 
-             <Components.OverlayContainer signinIn={signIn}>
-                 <Components.Overlay signinIn={signIn}>
+        axios.post('http://localhost:5000/api/auth/login', {
+            email,
+            password
+        })
+        .then(response => {
+            console.log(response.data);  
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token);
+            }
+        })
+        .catch(error => console.log('Error:', error));
+    }
 
-                 <Components.LeftOverlayPanel signinIn={signIn}>
-                     <Components.Title>Welcome Back!</Components.Title>
-                     <Components.Paragraph>
-                         To keep connected with us please login with your personal info
-                     </Components.Paragraph>
-                     <Components.GhostButton onClick={() => toggle(true)}>
-                         Sign In
-                     </Components.GhostButton>
-                     </Components.LeftOverlayPanel>
+    function handleRegister(event) {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
 
-                     <Components.RightOverlayPanel signinIn={signIn}>
-                       <Components.Title>Hello, Friend!</Components.Title>
-                       <Components.Paragraph>
-                           Enter Your personal details and start journey with us
-                       </Components.Paragraph>
-                           <Components.GhostButton onClick={() => toggle(false)}>
-                               Sigin Up
-                           </Components.GhostButton> 
-                     </Components.RightOverlayPanel>
- 
-                 </Components.Overlay>
-             </Components.OverlayContainer>
+        axios.post('http://localhost:5000/api/auth/register', {
+            name,
+            email,
+            password
+        })
+        .then(response => {
+            console.log('User registered', response.data);
+        })
+        .catch(error => console.log('Error:', error));
+    }
 
-         </Components.Container>
-     )
+    return (
+        <Components.Container>
+            <Components.SignUpContainer signinIn={!signIn}>
+                <Components.Form onSubmit={handleRegister}>
+                    <Components.Title>Create Account</Components.Title>
+                    <Components.Input type='text' name='name' placeholder='Name' />
+                    <Components.Input type='email' name='email' placeholder='Email' />
+                    <Components.Input type='password' name='password' placeholder='Password' />
+                    <Components.Button type="submit">Sign Up</Components.Button>
+                </Components.Form>
+            </Components.SignUpContainer>
+
+            <Components.SignInContainer signinIn={signIn}>
+                <Components.Form onSubmit={handleLogin}>
+                    <Components.Title>Sign in</Components.Title>
+                    <Components.Input type='email' name='email' placeholder='Email' />
+                    <Components.Input type='password' name='password' placeholder='Password' />
+                    <Components.Anchor href='#'>Forgot your password?</Components.Anchor>
+                    <Components.Button type="submit">Sign In</Components.Button>
+                </Components.Form>
+            </Components.SignInContainer>
+
+            <Components.OverlayContainer signinIn={signIn}>
+                <Components.Overlay signinIn={signIn}>
+                    <Components.LeftOverlayPanel signinIn={signIn}>
+                        <Components.Title>Welcome Back!</Components.Title>
+                        <Components.Paragraph>
+                            To keep connected with us please login with your personal info
+                        </Components.Paragraph>
+                        <Components.GhostButton onClick={() => toggle(true)}>
+                            Sign In
+                        </Components.GhostButton>
+                    </Components.LeftOverlayPanel>
+
+                    <Components.RightOverlayPanel signinIn={!signIn}>
+                        <Components.Title>Hello, Friend!</Components.Title>
+                        <Components.Paragraph>
+                            Enter your personal details and start your journey with us
+                        </Components.Paragraph>
+                        <Components.GhostButton onClick={() => toggle(false)}>
+                            Sign Up
+                        </Components.GhostButton>
+                    </Components.RightOverlayPanel>
+                </Components.Overlay>
+            </Components.OverlayContainer>
+        </Components.Container>
+    );
 }
 
 export default login;
